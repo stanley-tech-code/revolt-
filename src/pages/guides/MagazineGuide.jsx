6498f06344282e44ef76563ec4610ab6114c5950
stanley-tech-code <stanmbugua6@gmail.com>
2026-06-15 08:48:52 +0300

    fix: Add missing closing div tag in MagazineGuide.jsx to resolve Vite build error

--- src/pages/guides/MagazineGuide.jsx
+++ src/pages/guides/MagazineGuide.jsx
@@ -259,6 +259,7 @@ const MagazineGuide = ({ pageKey }) => {
           </section>
         );
       })}
+      </div>
 
       {/* SHOP BY SECTION */}
       {shopByVisible && shopByCards.length > 0 && (
